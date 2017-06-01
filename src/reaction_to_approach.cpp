bfacc25be5f9a1a2e92c9903cf1012b89469d1e6
yamasakiyuta <ikasama.y.cboxman@gmail.com>
2017-06-02 00:05:07 +0900

    可視化用のマーカを追加

--- src/reaction_to_approach.cpp
+++ src/reaction_to_approach.cpp
@@ -45,6 +45,7 @@ double pre_dist=0;
 double pre_x=0;
 const static double x_thresh=0.005;
 const static double vel_x=0.6;
+const static double range[5][2]={{5,-0.3},{5,-3.0},{0,-3.0},{0,-0.3},{5,-0.3}};
 
 double distance(double x1, double y1, double x2, double y2)
 {
@@ -56,6 +57,9 @@ void peopleCallback (const people_msgs::PositionMeasurementArray::ConstPtr& msg)
     people = *msg;
 }
 
+void search_range_marker(const visualization_msgs::Marker& line_strip){
+}
+        
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "reaction_to_approach");
@@ -63,6 +67,8 @@ int main(int argc, char **argv)
     ros::NodeHandle public_node_handle;
 
     ros::Publisher vel_pub = public_node_handle.advertise<geometry_msgs::Twist>("cmd_vel", 100);
+    ros::Publisher search_range_pub = public_node_handle.advertise<visualization_msgs::Marker>("search_range_marker", 10);
+    ros::Publisher tracking_pub = public_node_handle.advertise<visualization_msgs::Marker>("my_tracking_marker", 10);
     ros::Subscriber laser_sub = public_node_handle.subscribe<people_msgs::PositionMeasurementArray>("people_tracker_measurements", 1, peopleCallback);
     
     tf::TransformListener listener;
@@ -71,6 +77,44 @@ int main(int argc, char **argv)
 
     while (ros::ok())
     {
+        ////////////visualization/////////////////////////////////////////
+        visualization_msgs::Marker line_strip;
+        line_strip.header.frame_id = "/odom";
+        line_strip.header.stamp = ros::Time::now();
+        line_strip.ns = "lines";
+        line_strip.action = visualization_msgs::Marker::ADD;
+        line_strip.pose.orientation.w = 1.0;
+
+        line_strip.id = 1;
+        line_strip.type = visualization_msgs::Marker::LINE_STRIP;
+        line_strip.scale.x = 0.1;
+        // Line strip is blue
+        line_strip.color.b = 1.0;
+        line_strip.color.a = 1.0;
+        for(int i=0;i<5;i++){
+            geometry_msgs::Point p;
+            p.x = range[i][0];
+            p.y = range[i][1];
+            p.z = 0;
+            line_strip.points.push_back(p);
+        }
+        search_range_pub.publish(line_strip);
+        //////////////////////////////////////////////
+        visualization_msgs::Marker points;
+        points.header.frame_id = "/odom";
+        points.header.stamp = ros::Time::now();
+        points.ns = "points";
+        points.action = visualization_msgs::Marker::ADD;
+        points.pose.orientation.w = 1.0;
+        points.id = 0;
+        points.type = visualization_msgs::Marker::POINTS;
+        points.scale.x = 0.5;
+        points.scale.y = 0.5;
+        // Line strip is red
+        points.color.r = 1.0;
+        points.color.a = 1.0;
+        
+
         tf::StampedTransform transform;
         try{
             listener.lookupTransform("/odom", "/base_link", ros::Time(0), transform);
@@ -86,16 +130,21 @@ int main(int argc, char **argv)
         cmd_vel.linear.x=0;
     }
     else{
-        ROS_INFO("%d\n",people.people.size());
+        //ROS_INFO("%d\n",people.people.size());
         for(int i=0;i<=people.people.size();i++){
             double x = people.people[i].pos.x;
             double y = people.people[i].pos.y;
             double xGL = x+transform.getOrigin().x();
             double yGL = y+transform.getOrigin().y();
             ROS_INFO("%g, %g\n",x,y);
-            if(xGL>-5 && xGL<5 && yGL>-2.0 && yGL<-0.3){
+            if(xGL>range[2][0] && xGL<range[0][0] && yGL>range[1][1] && yGL<range[3][1]){
             //if(x>-5 && x<5 && y>-2.0 && y<-0.3){
                 dist = distance(0,0,x,y);
+                geometry_msgs::Point p;
+                p.x = xGL;
+                p.y = yGL;
+                p.z = 0;
+                points.points.push_back(p);
                 //ROS_INFO("dist=%g\n",dist);
                 //ROS_INFO("x=%g\n",transform.getOrigin().x());
                 //ROS_INFO("y=%g\n",transform.getOrigin().y());
@@ -140,15 +189,15 @@ int main(int argc, char **argv)
                 break;
             }
             else if(x>0 && x<2 && y>-0.5 && y<0.5){
-                cmd_vel.linear.x=-vel_x;
+                cmd_vel.linear.x=-0.2;
                 break;
             }
             else{
                 cmd_vel.linear.x=0;
                 ROS_INFO("stop(no legs)\n");
-                break;
             }
         }
+        tracking_pub.publish(points);
     }
         
         vel_pub.publish(cmd_vel);
